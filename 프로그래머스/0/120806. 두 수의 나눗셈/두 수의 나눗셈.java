184ad64841ed10953895b7223ece4ed9e5747fe3
SeulGi Lee <103404657+sgg917@users.noreply.github.com>
2026-04-20 18:46:59 +0900

    [level 0] Title: 두 수의 나눗셈, Time: 0.03 ms, Memory: 83.5 MB -BaekjoonHub

--- 프로그래머스/0/120806. 두 수의 나눗셈/두 수의 나눗셈.java
+++ 프로그래머스/0/120806. 두 수의 나눗셈/두 수의 나눗셈.java
@@ -1,8 +1,7 @@
 class Solution {
     public int solution(int num1, int num2) {
         int answer = 0;
-        float tmp = (float)num1 / (float)num2;
-        answer = (int)(tmp * 1000);
+        answer = (int)(((double)num1 / (double)num2) * 1000);
         return answer;
     }
 }
