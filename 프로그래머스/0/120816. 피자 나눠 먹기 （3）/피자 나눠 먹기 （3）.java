5924ca70cd32a895366485a2bbca18467e7cdea7
SeulGi Lee <103404657+sgg917@users.noreply.github.com>
2026-04-28 18:50:16 +0900

    [level 0] Title: 피자 나눠 먹기 (3), Time: 0.03 ms, Memory: 86 MB -BaekjoonHub

--- 프로그래머스/0/120816. 피자 나눠 먹기 （3）/피자 나눠 먹기 （3）.java
+++ 프로그래머스/0/120816. 피자 나눠 먹기 （3）/피자 나눠 먹기 （3）.java
@@ -1,9 +1,5 @@
 class Solution {
     public int solution(int slice, int n) {
-        if(slice >= n){
-            return 1;
-        }else{
-            return n%slice == 0 ? n/slice : (n/slice)+1;
-        }
+        return n%slice == 0 ? n/slice : (n/slice)+1;
     }
 }
