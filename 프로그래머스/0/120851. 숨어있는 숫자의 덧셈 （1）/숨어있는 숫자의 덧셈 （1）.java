ee7271085641ea32fb8a4a39c2e685aa9ccc1e62
SeulGi Lee <103404657+sgg917@users.noreply.github.com>
2026-05-05 22:15:24 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (1), Time: 1.14 ms, Memory: 85.8 MB -BaekjoonHub

--- 프로그래머스/0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.java
+++ 프로그래머스/0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.java
@@ -1,13 +1,9 @@
-import java.util.*;
-
 class Solution {
     public int solution(String my_string) {
         int answer = 0;
-        String str = "";
-        for(int i=0; i<my_string.length(); i++){
-            str = my_string.substring(i, i+1);
-            if(str.matches("[0-9]")){
-                answer += Integer.parseInt(str);
+        for(char c : my_string.toCharArray()){
+            if(String.valueOf(c).matches("[0-9]")){
+                answer += Integer.parseInt(String.valueOf(c));
             }
         }
         return answer;
